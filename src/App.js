72c677af93be4710eec4d470abc6c45cb6cc3ecd
Ndichu-shee <ndichujoyce8@gmail.com>
2020-09-23 23:32:05 +0300

    spreadsheet

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Spread from '.components/Table'
+
+
+const App = () =>(
+  <div style ={{width:'max-content'}}>
+    <Table x={4}/>
+  </div>
+)
+
+export default App;
---
+++ src/Spread.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Row from '.Row'
+
+export default class Table extends React.Component{
+    constructor (props)
+    super(props)
+
+    this.state = {
+        data:{}
+
+    }
+} 
+handleChangedCell =({x,y},  value)=>{
+    const modifiedData = Object.assign({} this.state.data)
+    if (!modifiedData[y]) modifiedDta[y] ={}
+    
+}
+updateCells = () =>{
+    this.forceUpdate()
+}
+render (){
+    const rows = []
+}
+for (let y=0; < this.props.y + 1; y +=1){
+    cons rowData = this.state.data[y] | | {}
+
+    rows.pus(
+        <Row
+        handleChangedCell={this.handleChangedCell}
+        updateCells={this.updateCells}
+        key={y}
+        y={y}
+        x={this.props.x + 1}
+        rowData={rowData}
+        />
+    )
+}
+return <div>{rows}</div>
+}
+}
+export default Row
